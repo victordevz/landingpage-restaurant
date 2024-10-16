a9ef3535f741aa04212c31b7e6fdb3e3b7af4645
Victor Correia <162618994+victordevz@users.noreply.github.com>
2024-10-16 10:41:29 -0300

    navbarv1
    construção da navbar e barra lateral em andamento

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar"
+
+const Home = () => {
+    return(
+        <div ClassName="home-container">
+            <Navbar/>
+
+        </div>
+    )
+};
+
+export default Home;
